6e20d282a6c1a77f0786dc37eb36a102c2afb9f8
69319010120-source <69319010120@techno-chon.ac.th>
2026-07-24 12:53:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const client = await pool.connect();
 const result = await client.query('SELECT * FROM students');
 client.release(); // คนืการเชื่อมตอเมื่อใชงานเสร็จ
 // 4. นําขอมูลที่ได(result.rows) มาประกอบเปนตาราง HTML
-let html = `<h1>ฐานขอมูลนักศึกษา (ทดสอบการเชื่อมตอ)</h1>`;
+let html = `<h1>ฐานข้อมูลนักศึกษา (ทดสอบการเชื่อมต่อ)</h1>`;
 html += `<table border="1" cellpadding="10">`;
-html += `<tr><th>รหัสนักศึกษา</th><th>ขื่อ-สกุล</th></tr>`;
+html += `<tr><th>69319010120</th><th>นางสาวธัญญาภรณ์ มีเณร</th></tr>`;
 // วนลูปนําขอมูลแตละแถวมาแสดง
 result.rows.forEach(row => {
 html += `<tr><td>${row.student_id}</td><td>${row.student_name}</td></tr>`;
